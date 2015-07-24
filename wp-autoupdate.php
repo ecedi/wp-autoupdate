64fa524804fc84c30ff641ba90d0b5102d99ceb8
syg <sgogel@ecedi.fr>
2015-07-24 11:57:46 +0200

    initial version

---
+++ wp-autoupdate.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author  Sylvain Gogel <sgogel@ecedi.fr>
+ * @package Wordpress
+ * @license http://opensource.org/licenses/MIT MIT
+ *
+ * Plugin Name: wp-autoupdate
+ * Description: Disable WordPress autoupdates
+ * Version: 1.0.0
+ * Author: Sylvain Gogel <sgogel@ecedi.fr>
+ * Author URI: http://ecedi.fr
+ */
+
+/**
+ *
+ * @since 1.0
+ */
+
+define("WP_AUTO_UPDATE_CORE", false);
